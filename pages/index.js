9658e765098447dba928ebadeb5e5c529d0ec092
DANIELRAYNAUD <drdanielraynaud@medvixsaude.com>
2025-03-16 14:07:32 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>CRMJA - Plataforma em Construção 🚀</h1>
+            <p>Estamos preparando a melhor experiência de estudo!</p>
+        </div>
+    );
+}
