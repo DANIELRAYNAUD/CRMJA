87e159a8c8af3e9268d74b113b2ce69d1eabe270
DANIELRAYNAUD <drdanielraynaud@medvixsaude.com>
2025-03-16 14:28:10 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ export default function Home() {
         <div style={{ textAlign: "center", padding: "20px" }}>
             <h1>CRMJA - Plataforma em Construção 🚀</h1>
             <p>Estamos preparando a melhor experiência de estudo!</p>
+            <p>🔄 Última atualização: Agora funcionando! 🔄</p>
         </div>
     );
 }
